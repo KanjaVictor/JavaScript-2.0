0ce3fb213ad037958ecae6726efa2fde2f84fe9d
KanjaVictor <victorkaguri77@gmail.com>
2020-11-20 23:52:59 +0300

    Initial Commit

---
+++ JavaScript.js
@@ -0,0 +1,18 @@
+//---Functions---
+
+//Output
+document.write(); //Its a function that writes texts into our HTML document.
+//Output to Console
+console.log(); //Its a function that performs output to the web console.
+
+alert();//Its used to create a message box.
+
+//Variables
+var cR = 7; //var keyword declares a variable, which are containers for storing data values.
+
+//Comments
+//Singleline Comments.This is an example of a Singleline comment.
+/*
+Multiline Comments
+This is an example of a Multiline comment.
+*/
