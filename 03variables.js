fe6c1ab9c3c5c05e3a46f514a84977a5aee62840
Kanja Victor <victorkaguri77@gmail.com>
2023-07-30 21:16:43 +0300

    Initial Commit

--- 03variables.js
+++ 03variables.js
@@ -19,12 +19,12 @@ If you use outside of a function - global scope.
 If you use inside of a function - it belongs to that function.
 */
 
-let cr = 7;//Has a block scope.
+let cr = 7;//Has a block scope. Is mutable and uses camelCase.
 /*
 Scoped version of var.
 */
 
-const cr =7;//Has a block scope.
+const CR =7;//Has a block scope. Is immutable and written in full uppercase.
 /*
 A variable once created, the value can never change. Its read-only.
 */
---
+++ 05Operator.js
@@ -0,0 +1,15 @@
+var add = 1 + 1;
+
+var subtract = 2 - 1;
+
+var multiply = 2 * 2;
+
+var divide = 10 / 5;
+
+var increment = 10;
+increment++; //This is equivalent to i + 1;
+
+var decrement = 10;
+decrement--; //This is equivalent to d - 1;
+
+var remainder = 48 % 2;//The answer would be 0. Since we're looking at the remainder.
